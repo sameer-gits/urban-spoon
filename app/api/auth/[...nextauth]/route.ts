adcacc668eda7baea95b49ceda6a7ae98eb0b470
sameer-gits <ms3054825@gmail.com>
2024-02-18 12:26:48 +0530

    prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,28 @@
+// import NextAuth from "next-auth";
+// import GitHubProvider from "next-auth/providers/github";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter"; // its older version bacause newer version is not working as of today it should be npm install @auth/prisma-adapter but its not working so using npm install @next-auth/prisma-adapter
+// import prisma from "@/lib/prisma";
+
+// export const authOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_ID ?? "",
+//       clientSecret: process.env.GITHUB_SECRET ?? "",
+//     }),
+//   ],
+// };
+
+// export const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
+
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"; // its older version bacause newer version is not working as of today it should be npm install @auth/prisma-adapter but its not working so using npm install @next-auth/prisma-adapter
 import prisma from "@/lib/prisma";
 
-export const OPTIONS = {
+export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
@@ -13,6 +32,6 @@ export const OPTIONS = {
   ],
 };
 
-export const handler = NextAuth(OPTIONS);
+export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 import AuthButton from "@/components/AuthButton";
 import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
 
 export default async function Home() {
-  const prismaUser = await prisma.user.findUnique({
-    where: { email: session.user.email },
-  });
+  const session = await getServerSession();
 
+  const prismaUser = await prisma.user.findUnique({
+    where: { email: session?.user?.email as string },
+  });
+  console.log(session, prismaUser);
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
+      {session?.user?.name ? (
+        <div>{session.user.name}</div>
+      ) : (
+        <div>Not logged in</div>
+      )}
       <AuthButton />
     </main>
   );
