6406aaa2f2501be3201a6c39dd5ef5321320ee33
sameer-gits <ms3054825@gmail.com>
2024-02-18 17:12:36 +0530

    prisma client working with jwt auth options in api

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,10 @@
-// import NextAuth from "next-auth";
-// import GitHubProvider from "next-auth/providers/github";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter"; // its older version bacause newer version is not working as of today it should be npm install @auth/prisma-adapter but its not working so using npm install @next-auth/prisma-adapter
-// import prisma from "@/lib/prisma";
-
-// export const authOptions = {
-//   adapter: PrismaAdapter(prisma),
-//   providers: [
-//     GitHubProvider({
-//       clientId: process.env.GITHUB_ID ?? "",
-//       clientSecret: process.env.GITHUB_SECRET ?? "",
-//     }),
-//   ],
-// };
-
-// export const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
-
 import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@next-auth/prisma-adapter"; // its older version bacause newer version is not working as of today it should be npm install @auth/prisma-adapter but its not working so using npm install @next-auth/prisma-adapter
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
@@ -30,6 +12,9 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 };
 
 export const handler = NextAuth(authOptions);
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
 import AuthButton from "@/components/AuthButton";
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 
 export default async function Home() {
   const session = await getServerSession();
 
-  const prismaUser = await prisma.user.findUnique({
-    where: { email: session?.user?.email as string },
-  });
-  console.log(session, prismaUser);
+  console.log("hello" + session);
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
+      <p className="text-2xl pb-6">getServerSession Result</p>
       {session?.user?.name ? (
         <div>{session.user.name}</div>
       ) : (
-        <div>Not logged in</div>
+        <div>Not logged in returning null</div>
       )}
+
       <AuthButton />
     </main>
   );
